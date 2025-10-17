f41c5742002005119290d894db006be1b4b9a0ad
Pim Kerkhoven <pimk@cs.umu.se>
2025-10-17 09:27:54 +0200

    Add test for non-numerical attribute sorting

--- test/unit/norms/test_rank_norm.py
+++ test/unit/norms/test_rank_norm.py
@@ -35,8 +35,6 @@ def test_rank_call_norm_function_call_non_existent_attribute():
     )
 
 
-# test non-numerical output for norm function attribute
-#
 # #TODO: rename norm function to value function? -> Check paper?
 
 
@@ -132,6 +130,29 @@ def test_rank_call_eight_items_complex():
 
     data = pd.DataFrame(
         data={"attr": [8, 5, 3, 7, 5, 4, 2, 4]},
+        index=["A", "B", "C", "D", "E", "F", "G", "H"],
+    )
+    outcome_scores = [6, 8, 3, 4, 3, 6, 5, 2]
+
+    norm = RankNorm(1, attr_norm_function)
+    result = norm(data, None, outcome_scores)
+
+    assert result["A"] == 1
+    assert result["B"] == 0
+    assert result["C"] == 1
+    assert result["D"] == 3
+    assert result["E"] == 2
+    assert result["F"] == 0
+    assert result["G"] == 0
+    assert result["H"] == 2
+
+
+def test_rank_call_non_numerical_norm_function():
+    def attr_norm_function(x):
+        return x["attr"]
+
+    data = pd.DataFrame(
+        data={"attr": ["H", "E", "C", "G", "E", "D", "B", "D"]},
         index=["A", "B", "C", "D", "E", "F", "G", "H"],
     )
     outcome_scores = [6, 8, 3, 4, 3, 6, 5, 2]
